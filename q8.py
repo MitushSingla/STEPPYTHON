c434a28a12143728c8a54e63aeae3dcbd983cf46
MitushSingla <95280881+MitushSingla@users.noreply.github.com>
2022-07-20 00:37:23 +0530

    Delete q8.py

--- q8.py
+++
@@ -1,9 +0,0 @@
-print("Prime number program in pythom:")
-num=6
-if num>1:
-    for i in range(2,num):
-      if num%i==0:
-        print(num,"is not a prime number")
-        break
-      else:
-        print(num,"is a prime number")
