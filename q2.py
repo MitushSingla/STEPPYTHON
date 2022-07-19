f86105ab0ea7e92fffdbfc756df1015e09f7bf58
MitushSingla <95280881+MitushSingla@users.noreply.github.com>
2022-07-20 00:40:39 +0530

    Delete q2.py

--- q2.py
+++
@@ -1,8 +0,0 @@
-print("Factorial Program")
-def factorial_recursive(n):
-    if n==0 or n==1:
-        return 1
-    else:
-        return n*factorial_recursive(n-1)
-f=factorial_recursive(3)
-print(f)
